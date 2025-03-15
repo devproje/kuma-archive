a2f00ac83ee1b393b7847cbbbc947d58c98a66de
Project_IO <me@projecttl.net>
2025-03-15 14:06:23 +0900

    feat: middle save

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router";
-import Dashboard from "./components/dashboard";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router";
+import { usePath } from "./store/path";
+import { useEffect, useState } from "react";
 
 import "./App.scss";
+import kuma from "./assets/kuma.png";
+import { Menu } from "lucide-react";
 
 function App() {
 	return (
@@ -10,6 +13,46 @@ function App() {
 				<Route path={"*"} element={<Dashboard />} />
 			</Routes>
 		</BrowserRouter>
+	);
+}
+
+function Dashboard() {
+	const path = usePath();
+	const location = useLocation();
+	const [load, setLoad] = useState(false);
+
+	useEffect(() => {
+		if (!load) {
+			path.update(location.pathname.substring(1, location.pathname.length));
+				setLoad(true);
+			}
+	
+			const id = setInterval(() => {
+				path.update(location.pathname.substring(1, location.pathname.length));
+			}, 5000);
+	
+		return () => clearInterval(id);
+	}, [load, path, location]);
+
+	return (
+		<main className="container-md ka-view">
+			<Header />
+		</main>
+	);
+}
+
+function Header() {
+	return (
+		<nav className="ka-nav">
+			<div className="title">
+				<img src={kuma} alt="" />
+				<h4 className="title-content">Kuma Archive</h4>
+			</div>
+
+			<button>
+				<Menu />
+			</button>
+		</nav>
 	);
 }
 

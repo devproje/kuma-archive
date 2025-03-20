ca8300b27cb7a9db8eb47685531b51bd9c921add
Project_IO <me@projecttl.net>
2025-03-20 17:51:28 +0900

    feat: middle save

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -2,9 +2,29 @@ import { useLocation } from "react-router";
 
 export function FileNavigator() {
 	const location = useLocation();
-	const split = location.pathname.substring(1, location.pathname.length).split("/");
+	const split = location.pathname === "/" ? Array<string>() : location.pathname.substring(1, location.pathname.length).split("/");
 
-	return <div>
-		
-	</div>;
+	return (
+		<div>
+			<a href="/">
+				<span>Index Directory</span>
+			</a>
+			{split.map((path, i) => {
+				if (i === split.length - 1) {
+					return (
+						<a key={i}>
+							<span>{path}</span>
+						</a>
+					);
+				}
+
+				return (
+					<a key={i} href="">
+						<span>{path}</span>
+						<span>&gt;&gt;</span>
+					</a>
+				);
+			})}
+		</div>
+	);
 }
